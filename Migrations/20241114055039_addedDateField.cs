8a22067748d5e44b1107d55f22d446467faf476b
Nitesh-mint <nitesh.raya058@gmail.com>
2024-11-14 16:02:37 +0545

    added pagination and searching features with some visual changes

---
+++ Migrations/20241114055039_addedDateField.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedDateField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TodoItems",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "TodoItems",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "TodoItems");
+        }
+    }
+}
